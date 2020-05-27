5fe704478d49e2eca7cffb273d73ed5a6c9aab5b
Christopher Pearson <cpearson3140@gmail.com>
2020-05-27 21:14:11 +0100

    Updated Ireland & IrelandTest files
    Signed-off-by: Christopher Pearson <cpearson3140@gmail.com>

--- src/main/java/Ireland.java
+++ src/main/java/Ireland.java
@@ -1,2 +1,43 @@
 public class Ireland {
+
+    private String coach;
+    private String stadium;
+    private int capacity;
+    private int championships;
+    private int millenniumTrophies;
+    private int centenaryQuiach;
+
+    public Ireland(String coach, String stadium, int capacity, int championships, int millenniumTrophies, int centenaryQuiach){
+        this.coach = coach;
+        this.stadium = stadium;
+        this.capacity = capacity;
+        this.championships = championships;
+        this.millenniumTrophies = millenniumTrophies;
+        this.centenaryQuiach = centenaryQuiach;
+    }
+
+    public String getCoach(){
+        return this.coach;
+    }
+
+    public String getStadium(){
+        return this.stadium;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public int getChampionships(){
+        return this.championships;
+    }
+
+    public int getMillenniumTrophies(){
+        return this.millenniumTrophies;
+    }
+
+    public int getCentenaryQuiach(){
+        return this.centenaryQuiach;
+    }
+
 }
--- src/test/java/IrelandTest.java
+++ src/test/java/IrelandTest.java
@@ -1,2 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class IrelandTest {
+
+    private Ireland ireland;
+
+    @Before
+    public void before(){
+        ireland = new Ireland("Andy Farrell", "Aviva Stadium", 51700, 14,13, 17);
+    }
+
+    @Test
+    public void getCoachName(){
+        assertEquals("Andy Farrell", ireland.getCoach());
+    }
+
+    @Test
+    public void getStadiumName(){
+        assertEquals("Aviva Stadium", ireland.getStadium());
+    }
+
+    @Test
+    public void getCapacity(){
+        assertEquals(51700, ireland.getCapacity());
+    }
+
+    @Test
+    public void getChampionships(){
+        assertEquals(14, ireland.getChampionships());
+    }
+
+    @Test
+    public void getMillenniumTrophies(){
+        assertEquals(13, ireland.getMillenniumTrophies());
+    }
+
+    @Test
+    public void getCentenaryQuiach(){
+        assertEquals(17, ireland.getCentenaryQuiach());
+    }
 }
