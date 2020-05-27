cdb3c84525777ed1037684666d66cee00d4ee3ea
Christopher Pearson <cpearson3140@gmail.com>
2020-05-27 22:15:13 +0100

    Updated Wales & WalesTest files
    Signed-off-by: Christopher Pearson <cpearson3140@gmail.com>

--- src/main/java/Italy.java
+++ src/main/java/Italy.java
@@ -1,2 +1,36 @@
 public class Italy {
+
+    private String coach;
+    private String stadium;
+    private int capacity;
+    private int championships;
+    private int giuseppeGaribaldiTrophies;
+
+    public Italy(String coach, String stadium, int capacity, int championships, int giuseppeGaribaldiTrophies){
+        this.coach = coach;
+        this.stadium = stadium;
+        this.capacity = capacity;
+        this.championships = championships;
+        this. giuseppeGaribaldiTrophies = giuseppeGaribaldiTrophies;
+    }
+
+    public String getCoach() {
+        return this.coach;
+    }
+
+    public String getStadium() {
+        return this.stadium;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int getChampionships() {
+        return this.championships;
+    }
+
+    public int getGiuseppeGaribaldiTrophies() {
+        return this.giuseppeGaribaldiTrophies;
+    }
 }
--- src/test/java/ItalyTest.java
+++ src/test/java/ItalyTest.java
@@ -1,2 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ItalyTest {
+
+    private Italy italy;
+
+    @Before
+    public void before(){
+        italy = new Italy("Franco Smith", "Stadio Olympico", 72698, 0,2);
+    }
+
+    @Test
+    public void getCoachName(){
+        assertEquals("Franco Smith", italy.getCoach());
+    }
+
+    @Test
+    public void getStadiumName(){
+        assertEquals("Stadio Olympico", italy.getStadium());
+    }
+
+    @Test
+    public void getCapacity(){
+        assertEquals(72698, italy.getCapacity());
+    }
+
+    @Test
+    public void getChampionships(){
+        assertEquals(0, italy.getChampionships());
+    }
+
+    @Test
+    public void getGiuseppeGaribaldiTrophies(){
+        assertEquals(2, italy.getGiuseppeGaribaldiTrophies());
+    }
 }
