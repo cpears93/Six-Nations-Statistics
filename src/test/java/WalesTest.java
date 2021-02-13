000d2a07a1904c75d12247b89ed15c70e8020537
cpears93 <cpearson3140@gmail.com>
2021-02-13 18:44:55 +0000

    Updated files

--- src/test/java/WalesTest.java
+++ src/test/java/WalesTest.java
@@ -9,7 +9,7 @@ public class WalesTest {
 
     @Before
     public void before(){
-        wales = new Wales("Wayne Pivac", "Principality Stadium", "Parc y Scarlets", 74500, 14870, 124, 39, 12, 21, 21, 2);
+        wales = new Wales("Wayne Pivac", "Principality Stadium", "Parc y Scarlets", 74500, 14870, 124, 39, 12, 21, 21, 3);
     }
 
     @Test
@@ -67,7 +67,7 @@ public class WalesTest {
 
     @Test
     public void getDoddieWeirCups(){
-        assertEquals(2,wales.getDoddieWeirCups());
+        assertEquals(3,wales.getDoddieWeirCups());
     }
 
 }
